b0428ad254e82a34430cb6326c1f6582ced33cb7
Nicole Shoblom <nicole.shoblom@gmail.com>
2025-03-16 14:19:37 +0900

    Update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static",
+    base:"/",
+    site: "https://nicoleshoblom.github.io"
+});
